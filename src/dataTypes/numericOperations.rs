0054c157bcf656a89401e7ef7004b3836a907682
Moctar Mamadou <bmaxtar@gmail.com>
2025-10-19 13:07:44 +0000

    Numeric Operations

---
+++ src/dataTypes/numericOperations.rs
@@ -0,0 +1,18 @@
+fn main() {
+    //Addition et soustraction
+    let sum = 5 + 15;
+    let difference = 95.5 - 12.3;
+
+    //Multiplication et division
+    let product = 4 * 3;
+    let quotient = 56.7 / 42.2;
+
+    //Reste de la division
+    let remainder = 63 % 5;
+
+    println!("Sum: {}", sum);
+    println!("Difference: {}", difference);
+    println!("Product: {}", product);
+    println!("Quotient: {}", quotient);
+    println!("Remainder: {}", remainder);
+}
