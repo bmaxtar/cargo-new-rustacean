88fe316af05fd72c7ea2e9f65be04b842b6cd9bb
Moctar Mamadou <bmaxtar@gmail.com>
2025-10-21 17:55:27 +0000

    End funtions

--- src/functions/functions.rs
+++ src/functions/functions.rs
@@ -1,65 +1,55 @@
-//Simple function definition and call
 fn main() {
+    // --- Simple function definition and call ---
     println!("Hello, world!");
+    print_another_function();
+    println!(); // for spacing
 
-    another_function();
+    // --- Function with parameters ---
+    let x_params = 5;
+    print_value(x_params);
+    println!();
+
+    // --- Function with a return value ---
+    let x_return = 5;
+    let result = double_value(x_return);
+    println!("The double of {} is {}", x_return, result);
+    println!();
+
+    // --- Another example of function parameters ---
+    print_two_params(20, 30);
+    println!();
+
+    // --- Statements and Expressions in Function Bodies ---
+    // A statement performs an action but does not return a value. `let y = ...;` is a statement.
+    let y = {
+        // This block is an expression. Its value is the value of the last line.
+        let x_expr = 3;
+        x_expr + 1 // No semicolon means this is an expression, and its value is returned.
+    };
+    println!("The value of y (from an expression block) is: {}", y);
+    println!();
+
+    // --- Functions with implicit return values ---
+    let x_five = five();
+    println!("The value of x (from five()) is: {}", x_five);
 }
 
-fn another_function() {
+fn print_another_function() {
     println!("Another function.");
-}
-
-// Function with parameters
-fn main_with_params() {
-    let x = 5;
-    print_value(x);
 }
 
 fn print_value(value: i32) {
     println!("The value is: {}", value);
 }
 
-// Function with return value
-fn main_with_return() {
-    let x = 5;
-    let result = double_value(x);
-    println!("The double of {} is {}", x, result);
-}
-
 fn double_value(value: i32) -> i32 {
-    value * 2
+    value * 2 // This is an expression, its value is returned.
 }
 
-fn main_fn_signatures() {
-    another_fn(20, 30);
-   
+fn print_two_params(x: i32, y: i32) {
+    println!("Function with two params: {}, {}", x, y);
 }
- 
-fn another_fn(x: i32, y: i32) {
-    println!("Another function with params: {}, {}", x, y);
-}
-
-//Statements and Expressions in Function Bodies
-fn main_fn_body() {
-
-    let x = 5;
-    // This is a statement
-    let y = {
-        // This is an expression
-        let x = 3;
-        x + 1
-    };
-
-    println!("The value of y is: {}", y);
-  
-}
-
-//Functions with Return Values
 
 fn five() -> i32 {
-    5
-}
-fn main() {
-    let x = five();
-    println!("The value of x is: {}", x);
+    5 // The value 5 is returned implicitly.
 }
